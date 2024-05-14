b6ffd16ec6a9e8a30efe70c9b9df79222526eb7e
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-05-15 01:40:12 +0900

    Messing with Helidon

--- src/main/java/com/autonomouslogic/esiproxy/Application.java
+++ src/main/java/com/autonomouslogic/esiproxy/Application.java
@@ -1,12 +1,36 @@
 package com.autonomouslogic.esiproxy;
 
-import io.micronaut.runtime.Micronaut;
+import io.helidon.config.Config;
+import io.helidon.webserver.ConnectionConfig;
+import io.helidon.webserver.WebServer;
+import io.helidon.webserver.http.HttpRouting;
+import lombok.extern.log4j.Log4j2;
 
 /**
  * Entry point for the stand-alone application.
  */
+@Log4j2
 public class Application {
 	public static void main(String[] args) {
-		Micronaut.build(args).banner(false).start();
+		log.info("Starting ESI Proxy");
+
+		//		Micronaut.build(args).banner(false).start();
+
+		var connectionConfig =
+				ConnectionConfig.builder().keepAlive(true).tcpNoDelay(true).build();
+		Config.builder().build();
+		WebServer.builder()
+				.port(8182)
+				.connectionConfig(connectionConfig)
+				.routing(Application::routing)
+				.build()
+				.start();
+	}
+
+	private static void routing(HttpRouting.Builder routing) {
+		routing.get("/greet", (req, res) -> {
+			log.info(req.path());
+			res.send("Hello World!\n");
+		});
 	}
 }
