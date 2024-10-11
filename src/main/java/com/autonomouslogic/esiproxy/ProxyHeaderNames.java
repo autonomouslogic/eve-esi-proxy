2180b2049a8d2a502bf85e873bcbdca2e29a1fb2
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-11 09:58:42 +0900

    refactor: Renamed project (#48)

--- src/main/java/com/autonomouslogic/esiproxy/ProxyHeaderNames.java
+++ src/main/java/com/autonomouslogic/esiproxy/ProxyHeaderNames.java
@@ -1,5 +1,5 @@
 package com.autonomouslogic.esiproxy;
 
 public class ProxyHeaderNames {
-	public static final String X_ESI_PROXY_CACHE_STATUS = "x-eve-esi-proxy-cache-status";
+	public static final String X_EVE_ESI_PROXY_CACHE_STATUS = "x-eve-esi-proxy-cache-status";
 }
