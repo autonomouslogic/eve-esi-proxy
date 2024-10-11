1a7ca49d484d313da7e55c57569d0970758e4a15
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-11 22:54:06 +0900

    Test cached responses aren't rate limited

--- src/test/java/com/autonomouslogic/eveesiproxy/handler/ProxyHandlerRateLimitTest.java
+++ src/test/java/com/autonomouslogic/eveesiproxy/handler/ProxyHandlerRateLimitTest.java
@@ -2,18 +2,29 @@ package com.autonomouslogic.eveesiproxy.handler;
 
 import static com.autonomouslogic.eveesiproxy.test.TestConstants.MOCK_ESI_PORT;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.autonomouslogic.eveesiproxy.EveEsiProxy;
+import com.autonomouslogic.eveesiproxy.http.ProxyHeaderNames;
+import com.autonomouslogic.eveesiproxy.http.ProxyHeaderValues;
 import com.autonomouslogic.eveesiproxy.test.DaggerTestComponent;
 import com.autonomouslogic.eveesiproxy.test.TestHttpUtils;
+import com.google.common.base.Stopwatch;
 import io.helidon.http.HeaderNames;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.OkHttpClient;
+import okhttp3.mockwebserver.Dispatcher;
+import okhttp3.mockwebserver.MockResponse;
 import okhttp3.mockwebserver.MockWebServer;
+import okhttp3.mockwebserver.RecordedRequest;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,6 +33,7 @@ import org.junitpioneer.jupiter.SetEnvironmentVariable;
 
 @SetEnvironmentVariable(key = "ESI_BASE_URL", value = "http://localhost:" + MOCK_ESI_PORT)
 @SetEnvironmentVariable(key = "ESI_USER_AGENT", value = "test@example.com")
+@SetEnvironmentVariable(key = "ESI_RATE_LIMIT_PER_S", value = "5")
 @Timeout(30)
 @Log4j2
 public class ProxyHandlerRateLimitTest {
@@ -31,11 +43,9 @@ public class ProxyHandlerRateLimitTest {
 	@Inject
 	OkHttpClient client;
 
-	@Inject
-	@Named("version")
-	String version;
-
 	MockWebServer mockEsi;
+
+	final Duration duration = Duration.ofSeconds(5);
 
 	@Inject
 	protected ProxyHandlerRateLimitTest() {}
@@ -45,6 +55,15 @@ public class ProxyHandlerRateLimitTest {
 	void setup() {
 		DaggerTestComponent.builder().build().inject(this);
 		mockEsi = new MockWebServer();
+		var expires =
+				DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now().plusHours(1));
+		mockEsi.setDispatcher(new Dispatcher() {
+			@NotNull
+			@Override
+			public MockResponse dispatch(@NotNull RecordedRequest recordedRequest) {
+				return new MockResponse().setResponseCode(200).setHeader("Expires", expires);
+			}
+		});
 		mockEsi.start(MOCK_ESI_PORT);
 		proxy.start();
 	}
@@ -52,89 +71,48 @@ public class ProxyHandlerRateLimitTest {
 	@AfterEach
 	@SneakyThrows
 	void stop() {
-		try {
-			TestHttpUtils.assertNoMoreRequests(mockEsi);
-		} finally {
-			proxy.stop();
-			mockEsi.shutdown();
+		proxy.stop();
+		mockEsi.shutdown();
+	}
+
+	@Test
+	@SneakyThrows
+	void shouldLimitRequests() {
+		var count = new AtomicInteger(0);
+		var watch = Stopwatch.createStarted();
+		while (watch.elapsed().compareTo(duration) < 0) {
+			TestHttpUtils.assertResponse(
+					TestHttpUtils.callProxy(
+							client, proxy, "GET", "/page", Map.of(HeaderNames.CACHE_CONTROL.lowerCase(), "no-cache")),
+					200,
+					Map.of(ProxyHeaderNames.X_EVE_ESI_PROXY_CACHE_STATUS, ProxyHeaderValues.CACHE_STATUS_MISS));
+			count.incrementAndGet();
 		}
+		var time = watch.elapsed().toMillis();
+		var rate = count.get() / (time / 1000.0);
+		log.info(String.format("Requests: %s, time: %s, rate: %.2f/s", count.get(), watch.elapsed(), rate));
+		assertEquals(5.0, rate, 1.5);
 	}
 
 	@Test
 	@SneakyThrows
-	void shouldUseConfiguredUserAgent() {
-		TestHttpUtils.enqueueResponse(mockEsi, 204);
-		var proxyResponse =
-				TestHttpUtils.callProxy(client, proxy, "GET", "/esi", Map.of(HeaderNames.USER_AGENT.lowerCase(), ""));
-		TestHttpUtils.assertResponse(proxyResponse, 204);
-
-		var esiRequest = TestHttpUtils.takeRequest(mockEsi);
-		assertEquals(
-				1,
-				esiRequest
-						.getHeaders()
-						.values(HeaderNames.USER_AGENT.lowerCase())
-						.size());
-		TestHttpUtils.assertRequest(
-				esiRequest,
-				"GET",
-				"/esi",
-				Map.of(HeaderNames.USER_AGENT.lowerCase(), "test@example.com eve-esi-proxy/" + version));
-	}
-
-	@Test
-	@SneakyThrows
-	void shouldForwardSuppliedUserAgent() {
-		TestHttpUtils.enqueueResponse(mockEsi, 204);
-		var proxyResponse = TestHttpUtils.callProxy(
-				client, proxy, "GET", "/esi", Map.of(HeaderNames.USER_AGENT.lowerCase(), "test-agent"));
-		TestHttpUtils.assertResponse(proxyResponse, 204);
-
-		var esiRequest = TestHttpUtils.takeRequest(mockEsi);
-		assertEquals(
-				1,
-				esiRequest
-						.getHeaders()
-						.values(HeaderNames.USER_AGENT.lowerCase())
-						.size());
-		TestHttpUtils.assertRequest(
-				esiRequest,
-				"GET",
-				"/esi",
-				Map.of(HeaderNames.USER_AGENT.lowerCase(), "test-agent test@example.com eve-esi-proxy/" + version));
-	}
-
-	@Test
-	@SetEnvironmentVariable(key = "ESI_USER_AGENT", value = "")
-	@SneakyThrows
-	void shouldForwardSuppliedUserAgentEvenIfOneIsntConfigured() {
-		TestHttpUtils.enqueueResponse(mockEsi, 204);
-		var proxyResponse = TestHttpUtils.callProxy(
-				client, proxy, "GET", "/esi", Map.of(HeaderNames.USER_AGENT.lowerCase(), "test-agent"));
-		TestHttpUtils.assertResponse(proxyResponse, 204);
-
-		var esiRequest = TestHttpUtils.takeRequest(mockEsi);
-		assertEquals(
-				1,
-				esiRequest
-						.getHeaders()
-						.values(HeaderNames.USER_AGENT.lowerCase())
-						.size());
-		TestHttpUtils.assertRequest(
-				esiRequest,
-				"GET",
-				"/esi",
-				Map.of(HeaderNames.USER_AGENT.lowerCase(), "test-agent eve-esi-proxy/" + version));
-	}
-
-	@Test
-	@SetEnvironmentVariable(key = "ESI_USER_AGENT", value = "")
-	@SneakyThrows
-	void shouldReturn400IfNoUserAgentAvailable() {
-		var proxyResponse =
-				TestHttpUtils.callProxy(client, proxy, "GET", "/esi", Map.of(HeaderNames.USER_AGENT.lowerCase(), ""));
-		TestHttpUtils.assertResponse(proxyResponse, 400, "User agent must be configured or header supplied");
-
-		TestHttpUtils.assertNoMoreRequests(mockEsi);
+	void shouldNotLimitCachedResponses() {
+		var count = new AtomicInteger(0);
+		var watch = Stopwatch.createStarted();
+		TestHttpUtils.assertResponse(
+				TestHttpUtils.callProxy(client, proxy, "GET", "/page"),
+				200,
+				Map.of(ProxyHeaderNames.X_EVE_ESI_PROXY_CACHE_STATUS, ProxyHeaderValues.CACHE_STATUS_MISS));
+		while (watch.elapsed().compareTo(duration) < 0) {
+			TestHttpUtils.assertResponse(
+					TestHttpUtils.callProxy(client, proxy, "GET", "/page"),
+					200,
+					Map.of(ProxyHeaderNames.X_EVE_ESI_PROXY_CACHE_STATUS, ProxyHeaderValues.CACHE_STATUS_HIT));
+			count.incrementAndGet();
+		}
+		var time = watch.elapsed().toMillis();
+		var rate = count.get() / (time / 1000.0);
+		log.info(String.format("Requests: %s, time: %s, rate: %.2f/s", count.get(), watch.elapsed(), rate));
+		assertTrue(rate > 10.0, "rate:" + rate);
 	}
 }
