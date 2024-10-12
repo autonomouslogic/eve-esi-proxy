6a1c5490774957321117cff4cf1967ab143ca3a6
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-12 11:17:06 +0900

    Format

--- src/main/java/com/autonomouslogic/eveesiproxy/http/EsiRelay.java
+++ src/main/java/com/autonomouslogic/eveesiproxy/http/EsiRelay.java
@@ -11,8 +11,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.time.Duration;
-import java.time.Duration;
-import java.util.Optional;
 import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
 import okhttp3.Cache;
