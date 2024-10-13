ddbe8bbef4021d93ed73ae831d5e5deaecafb127
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-14 07:53:50 +0900

    Interface

--- src/main/java/com/autonomouslogic/eveesiproxy/handler/IndexHandler.java
+++ src/main/java/com/autonomouslogic/eveesiproxy/handler/IndexHandler.java
@@ -1,5 +1,6 @@
 package com.autonomouslogic.eveesiproxy.handler;
 
+import com.autonomouslogic.eveesiproxy.oauth.AuthManager;
 import io.helidon.http.HeaderNames;
 import io.helidon.webserver.http.Handler;
 import io.helidon.webserver.http.HttpRules;
@@ -9,6 +10,7 @@ import io.helidon.webserver.http.ServerResponse;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import jakarta.inject.Singleton;
+import java.util.stream.Collectors;
 import lombok.extern.log4j.Log4j2;
 
 @Singleton
@@ -16,6 +18,9 @@ import lombok.extern.log4j.Log4j2;
 public class IndexHandler implements HttpService, Handler {
 	@Inject
 	protected StandardHeaders standardHeaders;
+
+	@Inject
+	protected AuthManager authManager;
 
 	@Inject
 	@Named("version")
@@ -33,9 +38,23 @@ public class IndexHandler implements HttpService, Handler {
 
 	@Override
 	public void handle(ServerRequest req, ServerResponse res) throws Exception {
+		// https://images.evetech.net/characters/1338057886/portrait
+		var authedCharacters = authManager.getAuthedCharacters();
+		var authedCharactersHtml = authedCharacters.isEmpty()
+				? "<i>No characters logged in</i>"
+				: "<ul>"
+						+ authedCharacters.stream()
+								.map(ac -> "<li>%s [%d] - proxy key: <pre>%s</pre></li>"
+										.formatted(ac.getCharacterName(), ac.getCharacterId(), ac.getProxyKey()))
+								.collect(Collectors.joining("\n"))
+						+ "</ul>";
+		var html = """
+			<h1>EVE ESI Proxy %s</h1>
+			%s
+			""".formatted(version, authedCharactersHtml);
 		standardHeaders
 				.apply(res)
 				.header(HeaderNames.CONTENT_TYPE.lowerCase(), "text/html")
-				.send("EVE ESI Proxy " + version + "\n<a href=\"/login\">Login</a>\n");
+				.send(html);
 	}
 }
