c1b6b175d729a3cdd45660e7cd13f9fc995c4077
Kenneth Jorgensen <kenneth@autonomouslogic.com>
2024-10-20 16:12:53 +0900

    CSS and preflight

--- src/main/java/com/autonomouslogic/eveesiproxy/inject/ThymeleafModule.java
+++ src/main/java/com/autonomouslogic/eveesiproxy/inject/ThymeleafModule.java
@@ -5,22 +5,42 @@ import dagger.Provides;
 import jakarta.inject.Singleton;
 import java.time.Duration;
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.jetbrains.annotations.NotNull;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Module
 public class ThymeleafModule {
 	@Singleton
 	@Provides
 	public TemplateEngine templateEngine() {
+		var engine = new TemplateEngine();
+		engine.addDialect(new LayoutDialect());
+		engine.addTemplateResolver(cssResolver());
+		engine.addTemplateResolver(htmlResolver());
+		return engine;
+	}
+
+	private static ITemplateResolver cssResolver() {
+		var resolver = new ClassLoaderTemplateResolver();
+		resolver.setCheckExistence(true);
+		resolver.setPrefix("/templates/");
+		resolver.setSuffix(".css");
+		resolver.setTemplateMode(TemplateMode.CSS);
+		resolver.setCacheable(true);
+		resolver.setCacheTTLMs(Duration.ofDays(1000).toMillis());
+		return resolver;
+	}
+
+	private static ITemplateResolver htmlResolver() {
 		var resolver = new ClassLoaderTemplateResolver();
 		resolver.setPrefix("/templates/");
 		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
 		resolver.setCacheable(true);
 		resolver.setCacheTTLMs(Duration.ofDays(1000).toMillis());
-		var engine = new TemplateEngine();
-		engine.addDialect(new LayoutDialect());
-		engine.addTemplateResolver(resolver);
-		return engine;
+		return resolver;
 	}
 }
--- src/main/java/com/autonomouslogic/eveesiproxy/ui/TemplateUtil.java
+++ src/main/java/com/autonomouslogic/eveesiproxy/ui/TemplateUtil.java
@@ -1,19 +1,30 @@
 package com.autonomouslogic.eveesiproxy.ui;
 
+import com.autonomouslogic.commons.ResourceUtil;
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Locale;
 import java.util.Map;
 import lombok.NonNull;
+import lombok.SneakyThrows;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
 @Singleton
 public class TemplateUtil {
+
+	static {
+	}
+
 	@Inject
 	protected TemplateEngine templateEngine;
 
 	@Inject
+	@SneakyThrows
 	protected TemplateUtil() {}
 
 	public String render(@NonNull String template, @NonNull Map<String, Object> model) {
@@ -25,5 +36,19 @@ public class TemplateUtil {
 		var context = new Context(Locale.ENGLISH);
 		model.forEach(context::setVariable);
 		return context;
+	}
+
+	@SneakyThrows
+	private static String loadString(String path) {
+		try (var in = ResourceUtil.loadResource("/templates/" + path)) {
+			return IOUtils.toString(in, StandardCharsets.UTF_8);
+		}
+	}
+
+	@SneakyThrows
+	private static String loadBase64(String path) {
+		try (var in = ResourceUtil.loadResource("/templates/" + path)) {
+			return Base64.encodeBase64String(IOUtils.toByteArray(in));
+		}
 	}
 }
